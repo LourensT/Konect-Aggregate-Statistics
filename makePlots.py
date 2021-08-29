8961e9207579638b43d522fd419247a5046fb965
LourensT <touwenlourens@gmail.com>
2021-08-29 16:11:54 +0200

    first batch of (unpolished plots)

--- makePlots.py
+++ makePlots.py
@@ -7,18 +7,73 @@ import matplotlib.pyplot as plt
 from TikzPlotsFromPython.GenerateTikz import GenerateTikz
 
 BASE_FP = os.getcwd() + "\\plots\\"
+size_min = 0
 
-df = pd.read_csv("dataset.csv")
-print(df.shape)
-print(df["Diameter"].describe())
+def make_plot(index, metric, xlog, ylog):
+    if xlog:
+        if ylog:
+            fp = ("log_" + index + "-log_" + metric + ".tikz").replace(" ", "_")
+        else:
+            fp = ("log_" + index + "-" + metric + ".tikz").replace(" ", "_")
+    else:
+        if ylog:
+            fp = (index + "-log_" + metric + ".tikz").replace(" ", "_")
+        else:
+            fp = (index + "-" + metric + ".tikz").replace(" ", "_")
 
-df = df[df["Category"] != "Infrastructure network" ]
+    print("++ " + fp)
+
+    # filter the dataframe
+    df = pd.read_csv("dataset.csv")
+    print("dataframe shape before filter", df.shape)
+    df = df[(df["Category"] != "Infrastructure network") & (df["Size"] >= size_min)]
+    print("dataframe shape after filter", df.shape)
+
+    df_dict = df.set_index(index)[metric].to_dict()
+    plot = GenerateTikz(BASE_FP + fp, documentation="Plot of Konect networks without Infrastructure networks and with size > {}, total number of networks {}".format(size_min, df.shape[0]))
+    plot.setConfiguration(0, max(df_dict.keys()), 0, max(df_dict.values()), xlog=xlog, ylog=ylog, xlabel=index, ylabel=metric)
+    plot.addSeries(df_dict, metric)
 
 
-df_dict = df.set_index("Size")["Maximum degree"].to_dict()
-plot = GenerateTikz(BASE_FP + "set.tikz", documentation="Log-log of size-maximum degree, on all networks ~Infrastructure network")
-plot.setConfiguration(0, max(df_dict.keys()), 0, max(df_dict.values()), xlog=True, ylog=True)
-plot.addSeries(df_dict, "Maximum Degree")
 
-df.plot.scatter(x="Size", y = "Maximum degree", logx=True, logy=True)
-plt.show()
+
+# N>1000(?)
+size_min = 1000
+# log netwerk size - log maximum degree
+make_plot("Size", "Maximum degree", True, True)
+# log netwerk size - log maximum indegree
+make_plot("Size", "Maximum indegree", True, True)
+
+size_min = 0
+# log netwerk size - log average degree
+make_plot("Size", "Average degree", True, True)
+# log netwerk size - log average indegree (0.5*average degree)
+# TODO decide to split up undirected vs directed logs
+
+# size N>1000(?)
+size_min = 1000
+# 	(ook zonder log proberen)
+# log(size)	- 50-Percentile effective diameter
+make_plot("Size", "50-Percentile effective diameter", True, False)
+# log(size)	- 90-Percentile effective diameter
+make_plot("Size", "90-Percentile effective diameter", True, False)
+
+# unipartite undirected
+# size - proportion van largest component (Relative size of LC)
+# TODO
+
+# directed
+# size - proportion van giant  component (Relative size of LSCC)
+# TODO
+
+# size N>1000(?)
+size_min = 1000
+# size 		- diameter	
+make_plot("Size", "Diameter", False, False)
+# log(size)	- diameter 	(verwacht lineare trend in wolk omhoog)
+make_plot("Size", "Diameter", True, False)
+
+# size N>1000(?)
+size_min = 1000
+# size 		- clustering coefficient	(real world meer clustering dan random graphs)
+make_plot("Size", "Clustering coefficient", False, False)
